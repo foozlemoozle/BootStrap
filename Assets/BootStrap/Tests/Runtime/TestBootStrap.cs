132570a9843d806f9dc92723a01c5ffdf578532e
Kirk George <kirk.e.george@gmail.com>
2020-05-26 17:25:31 -0700

    KEG first commit for BootStrap

---
+++ Assets/BootStrap/Tests/Runtime/TestBootStrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.keg.bootstrap;
+
+namespace com.keg.bootstrap.tests
+{
+    public class TestBootStrap : BootStrap
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Then( new TestManager() { name = "1", setupTimeMS = 500 } )
+                .Then( new TestManager() { name = "2a", setupTimeMS = 1000 } )
+                .And( new TestManager() { name = "2b", setupTimeMS = 2000 } )
+                .Then( new TestManager() { name = "3", setupTimeMS = 3000, teardownTimeMS = 500 } )
+                .InitManagers();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/BootStrap/Tests/Runtime/TestManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.keg.bootstrap;
+using System.Threading.Tasks;
+
+namespace com.keg.bootstrap.tests
+{
+    public class TestManager : IManager
+    {
+		public string name;
+		public int setupTimeMS;
+		public int teardownTimeMS;
+
+		public async Task Setup( BootStrap strap, System.Action<IManager> onSetup, System.Action<IManager> onFail )
+		{
+			Debug.LogFormat( "<color=green>{0}</color> start at {1}", name, Time.fixedTime );
+
+			Task setup = Task.Delay( setupTimeMS );
+			await setup;
+
+			Debug.LogFormat( "<color=green>{0}</color> end at {1}", name, Time.fixedTime );
+			onSetup( this );
+		}
+
+		public void Update()
+		{
+		}
+
+		public async Task Teardown( System.Action onTeardown )
+		{
+			Debug.LogFormat( "<color=red>{0}</color> start at {1}", name, Time.fixedTime );
+
+			Task teardown = Task.Delay( teardownTimeMS );
+			await teardown;
+
+			Debug.LogFormat( "<color=red>{0}</color> end at {1}", name, Time.fixedTime );
+			onTeardown();
+		}
+    }
+}
